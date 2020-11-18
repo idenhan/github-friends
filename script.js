2b62ae6b4a3370fcbe71c018429654042b288e3f
Iden Han <idenhan129@gmail.com>
2020-11-18 18:24:04 +0900

    Get the user data when its searched from github api

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ form.addEventListener("submit", function(e) {
   let search = document.getElementById("search").value;
   let originalName = search.split(" ").join("");
 
-  alert(originalName);
-  fetch("https://api.github.com/users/"+originalName); 
+  fetch("https://api.github.com/users/"+originalName)
+  .then((result) => result.json())
+  .then((data) => {
+    console.log(data);
+  })
 })
