b3823c214713054a11535c0715c8ddb70d34dcfc
Iden Han <idenhan129@gmail.com>
2020-11-18 18:45:06 +0900

    api test has done

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ form.addEventListener("submit", function(e) {
   let search = document.getElementById("search").value;
   let originalName = search.split(" ").join("");
 
+  document.getElementById("result").innerHTML = "";
+
   fetch("https://api.github.com/users/"+originalName)
   .then((result) => result.json())
   .then((data) => {
     console.log(data);
+    document.getElementById("result").innerHTML = `
+      <a target="_blank" href="https://www.github.com/${originalName}"><img src="${data.avatar_url}"/></a>
+    `
   })
+  // doubleathj
+  // InseobJeon
 })
