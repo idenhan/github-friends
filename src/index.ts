8f727ff1312ef17782459892031e472d88563302
Iden Han <idenhan129@gmail.com>
2020-11-21 05:25:33 +0900

    Make data do not stack

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as _ from "lodash";
 const form = document.getElementById("form");
 
 let user: string = "";
+let userData: any;
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -11,21 +12,28 @@ form.addEventListener("submit", function (e) {
 
   let originalName: string = username.split(" ").join("");
 
-  fetch("https://api.github.com/search/users?q=" + originalName + "+in:user&per_page=100")
-    .then((result: Response) => result.json())
-    .then((data: any) => {
-      console.log(data);
+  getUserData();
 
-      const userData = data.items;
-      userData.forEach((item: any) => {
-        user = `<a target="_blank" href="${item.html_url}"><img class="rounded-circle m-4" width="100" height="100" src="${item.avatar_url}"/></a>`;
+  function getUserData() {
+    document.getElementById("result").innerHTML = "";
+    fetch("https://api.github.com/search/users?q=" + originalName + "+in:user&per_page=100")
+      .then((result: Response) => result.json())
+      .then((data: any) => {
+        console.log(data);
 
-        let imageSpan: HTMLSpanElement = document.createElement("span");
-        imageSpan.innerHTML = user;
-        let result: HTMLElement = document.getElementById("result");
+        userData = data.items;
 
-        result.appendChild(imageSpan);
+        userData.forEach((item: any) => {
+          user = `<a target="_blank" href="${item.html_url}"><img class="rounded-circle m-4" width="100" height="100" src="${item.avatar_url}"/></a>`;
+
+          let imageSpan: HTMLSpanElement = document.createElement("span");
+          imageSpan.innerHTML = user;
+
+          let result: HTMLElement = document.getElementById("result");
+
+          result.appendChild(imageSpan);
+        })
       })
-    })
-
+  }
 })
+
