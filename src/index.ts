792f6b24253e8a879bbaef6b13dfc123812b65cd
Iden Han <idenhan129@gmail.com>
2020-11-21 04:22:10 +0900

    Fixed errors and Managed to print images of users searched in HTML

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,35 @@
 import "./styles.css";
 import * as _ from "lodash";
 
-// const form = document.getElementById("form");
+const form = document.getElementById("form");
 
-// let user;
+let user: string;
 
-// form.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   let username = (<HTMLInputElement>document.getElementById("username")).value;
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let username = (<HTMLInputElement>document.getElementById("username")).value;
 
-//   let originalName = username.split(" ").join("");
+  let originalName: string = username.split(" ").join("");
 
-//   fetch("https://api.github.com/search/users?q=" + originalName + "+in:user&per_page=100")
-//     .then((result) => result.json())
-//     .then((data) => {
-//       console.log(data);
+  fetch("https://api.github.com/search/users?q=" + originalName + "+in:user&per_page=100")
+    .then((result: Response) => result.json())
+    .then((data: any) => {
+      console.log(data);
 
-//       data.items.forEach((item: any) => {
-//         user = `<img class="img-thumbnail ml-4" src="${item.avatar_url}"/>`
-//         const result = document.getElementById("result");
+      const userData = data.items;
+      userData.forEach((item: any) => {
+        user = `<img class="img-thumbnail ml-4" src="${item.avatar_url}"/>`;
+        // document.getElementById("result").innerHTML = user;
+        let imageSpan = document.createElement("span");
+        // let imageNode = document.createTextNode(user);
+        // image.appendChild(imageNode);
+        imageSpan.innerHTML = user;
+        let result = document.getElementById("result");
+        result.appendChild(imageSpan);
+        // result.append(user);
+        console.log(user);
 
-//         result.append(user);
-//         console.log("힘들다");
+      })
+    })
 
-//       })
-//     })
-
-// })
+})
