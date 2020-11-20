166bf17a578fd1debb528b991103fd984056d0a4
Iden Han <idenhan129@gmail.com>
2020-11-21 07:57:20 +0900

    getting and posting API data is done

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,6 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
   let username = (<HTMLInputElement>document.getElementById("username")).value;
 
-  // let originalName: string = username;
-
   getUserData();
 
   function getUserData() {
@@ -24,15 +22,22 @@ form.addEventListener("submit", function (e) {
         userData = data.items;
 
         userData.forEach((item: any) => {
-          user = `<a target="_blank" href="${item.html_url}"><img class="rounded-circle m-4" width="100" height="100" src="${item.avatar_url}"/></a>`;
+          user = `<a target="_blank" href="${item.html_url}"><img class="rounded-circle" width="80" height="80" src="${item.avatar_url}"/></a>`;
 
-          let imageSpan: HTMLSpanElement = document.createElement("span");
-          imageSpan.className = "span-image"
+          let imageSpan: HTMLSpanElement = document.createElement("div");
+          imageSpan.className = "result-image mt-3 mb-3";
           imageSpan.innerHTML = user;
 
+          let nameSpan: HTMLSpanElement = document.createElement("span");
+          nameSpan.className = "result-name";
+          let nameSpanChild: Text = document.createTextNode(username);
+          nameSpan.appendChild(nameSpanChild);
+
+          imageSpan.appendChild(nameSpan);
           let result: HTMLElement = document.getElementById("result");
 
           result.appendChild(imageSpan);
+          // result.appendChild(nameSpan);
         })
       })
   }
