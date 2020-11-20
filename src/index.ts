8c07426919d2e994a699e3b43b16749b0c31d23c
Iden Han <idenhan129@gmail.com>
2020-11-20 14:01:27 +0900

    Create the searchbar

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,16 @@ import "./styles.css";
 import * as _ from "lodash";
 
 window.addEventListener("load", () => {
-    const header = document.createElement("h1");
-    header.innerText = "Github❤️Friends"
+  const header = document.createElement("h1");
+  header.innerText = "Github❤️Friends"
 
-    const body = document.querySelector("body");
-    body.appendChild(header);
+  const body = document.querySelector("body");
+  body.appendChild(header);
 
-    const food = [{ id: 0, name: "pizza" }, { id: 1, name: "sushi" }]
+  const food = [{ id: 0, name: "pizza" }, { id: 1, name: "sushi" }]
 
-    const foodElement = document.createElement("h2")
-    foodElement.innerText = _.find(food, { id: 0 })?.name;
+  const foodElement = document.createElement("h2")
+  foodElement.innerText = _.find(food, { id: 0 })?.name;
 
-    body.appendChild(foodElement);
+  body.appendChild(foodElement);
 })
