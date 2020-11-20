bc8fe15d3777782325c715cab7e0df7eb8f2a436
Iden Han <idenhan129@gmail.com>
2020-11-21 06:26:53 +0900

    list searched users by names

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,13 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
   let username = (<HTMLInputElement>document.getElementById("username")).value;
 
-  let originalName: string = username.split(" ").join("");
+  // let originalName: string = username;
 
   getUserData();
 
   function getUserData() {
     document.getElementById("result").innerHTML = "";
-    fetch("https://api.github.com/search/users?q=" + originalName + "+in:user&per_page=100")
+    fetch("https://api.github.com/search/users?q=" + username + "+in:name&per_page=100")
       .then((result: Response) => result.json())
       .then((data: any) => {
         console.log(data);
@@ -27,6 +27,7 @@ form.addEventListener("submit", function (e) {
           user = `<a target="_blank" href="${item.html_url}"><img class="rounded-circle m-4" width="100" height="100" src="${item.avatar_url}"/></a>`;
 
           let imageSpan: HTMLSpanElement = document.createElement("span");
+          imageSpan.className = "span-image"
           imageSpan.innerHTML = user;
 
           let result: HTMLElement = document.getElementById("result");
