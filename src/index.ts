df8558e2d924c47ae44f83de3bafe4dd10824c5c
Iden Han <idenhan129@gmail.com>
2020-11-21 22:56:53 +0900

    Solved github rate limit issue

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import "./styles.css";
 import * as _ from "lodash";
+import { MY_TOKEN_NUMBERS } from "./Constants/apiConstants";
+
+export default class AppRequestInit implements RequestInit {
+  public method: string = "GET";
+  public headers: Headers = new Headers();
+  public mode: RequestMode = "cors";
+
+  constructor() {
+    this.headers.set("Content-Type", "text/json;charset=utf-8");
+    this.headers.append("Authorization", `Bearer ${MY_TOKEN_NUMBERS}`);
+  }
+}
 
 const form = document.getElementById("form");
 
@@ -15,14 +27,14 @@ form.addEventListener("submit", function (e) {
 
   function getUserData() {
     document.getElementById("result").innerHTML = "";
-    fetch("https://api.github.com/search/users?q=" + username + "+in:name&per_page=100")
+    fetch("https://api.github.com/search/users?q=" + username + "+in:name&per_page=100", new AppRequestInit())
       .then((result: Response) => result.json())
       .then((data: any) => {
         console.log(data);
 
         userData = data.items;
 
-        userData.forEach((item: any) => {
+        userData.map((item: any) => {
           userImage = `<a target="_blank" href="${item.html_url}"><img class="rounded-circle" width="80" height="80" src="${item.avatar_url}"/></a>`;
           userName = item.login;
           console.log(userName);
@@ -31,30 +43,24 @@ form.addEventListener("submit", function (e) {
           imageSpan.className = "result-image mt-3 mb-3";
           imageSpan.innerHTML = userImage;
 
-          fetch("https://api.github.com/users/" + userName)
+          fetch("https://api.github.com/users/" + userName, new AppRequestInit())
             .then((nameResult: Response) => nameResult.json())
             .then((nameData: any) => {
               console.log(nameData);
 
-
               userName = nameData.name;
               console.log(userName);
+
               let nameSpan: HTMLSpanElement = document.createElement("span");
               nameSpan.className = "result-name";
               let nameSpanChild: Text = document.createTextNode(userName);
               nameSpan.appendChild(nameSpanChild);
+
               imageSpan.appendChild(nameSpan);
               let result: HTMLElement = document.getElementById("result");
 
               result.appendChild(imageSpan);
-
             })
-
-
-
-
-
-          // result.appendChild(nameSpan);
         })
       })
   }
